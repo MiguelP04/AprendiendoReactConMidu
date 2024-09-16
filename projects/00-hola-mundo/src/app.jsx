d722db1348d3b5a2fdb088862692455886be7a6f
Miguel Petit <miguel09112004@gmail.com>
2024-09-16 15:08:01 -0400

    Respaldo para cambiar a linux

---
+++ projects/00-hola-mundo/src/app.jsx
@@ -0,0 +1,8 @@
+import './app.css'
+import { TwitterFollowCard } from './TwiiterFollowCard'
+
+export function App (){
+    return (
+        < TwitterFollowCard name="Miguel Petit" userName="Miguel_PØ4"  />
+    )
+}
